36bb9ba85a811ba42c98bf4add603637c015228a
Mina <wilhelmina.jane@gmail.com>
2017-01-28 12:27:26 +0000

    adds the correct script file

--- public/script.js
+++ public/script.js
@@ -1,28 +1,78 @@
-$(document).ready(function() {
-    $.ajax({
-        url: '/get-posts',
-        dataType: 'json',
-        success: function(data) {
+if (document.readyState !== 'loading') {
+    ready();
+} else {
+    document.addEventListener('DOMContentLoaded', ready);
+}
 
-            for (var blogPost in data) {
-                var postDiv         = document.createElement('div');
-                var postText        = document.createElement('p');
-                var thumbnail       = document.createElement('img');
-                var postContainer   = document.getElementsByClassName('post-container')[0];
+function ready () {
+    getBlogposts('/get-posts');
 
-                thumbnail.src = "./img/logo2.png";
-                thumbnail.className = "thumbnail";
-                postText.innerHTML = data[blogPost];
-                postDiv.className = "post";
+    // send posts to server
+    var form = document.querySelector('form');
+    form.addEventListener('submit', function (event) {
 
-                postDiv.appendChild(thumbnail);
-                postDiv.appendChild(postText);
-                postContainer.appendChild(postDiv);
-                
-            }
-        },
-        error: function(error){
-            console.log(error);
-        }
+        event.preventDefault(); // prevents the form from contacting our server automatically (we want to do it ourselves)
+        var formActionUrl = form.action; // 'form.action' is the url '/create-post'
+        var formData = new FormData(form);
+
+        postBlogposts(formActionUrl, formData);
     });
-});
+}
+
+/****
+ * Function definitions
+ ***/
+function postBlogposts (url, data) {
+    fetch(url, {
+        method: 'POST',
+        body: data
+    })
+    .then(function (res) {
+        res.json()
+            .then(function (json) {
+                console.log(json);
+                addBlogpostsToPage(json);
+                document.querySelector('form').reset();
+        })
+    })
+    .catch(function (err) {
+        console.error(err)
+    });
+}
+
+function getBlogposts (url) {
+    fetch(url, {
+        method: 'GET'
+    })
+    .then(function (res) {
+        res.json()
+        .then(function (json) {
+            console.log(json);
+            addBlogpostsToPage(json);
+        });
+    })
+    .catch(function (err) {
+        console.error(err)
+    });
+}
+
+function addBlogpostsToPage (data) {
+    for (var blogpost in data) {
+        if (data.hasOwnProperty(blogpost)) {
+
+            var postDiv         = document.createElement('div');
+            var postText        = document.createElement('p');
+            var thumbnail       = document.createElement('img');
+            var postContainer   = document.querySelector('.post-container');
+
+            thumbnail.src = "./img/logo2.png";
+            thumbnail.className = "thumbnail";
+            postText.innerHTML = data[blogpost];
+            postDiv.className = "post";
+
+            postDiv.appendChild(thumbnail);
+            postDiv.appendChild(postText);
+            postContainer.appendChild(postDiv);
+        }
+    }
+}
